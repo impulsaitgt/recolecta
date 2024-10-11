9013553bc64834edf6b6e97b142b9d374259acb6
Alexander Paiz <wpaiz@grupotecun.com>
2024-10-11 09:50:35 -0600

    Ajuste para nombers de clientes largos

--- TecunRecolecta/clientes/forms.py
+++ TecunRecolecta/clientes/forms.py
@@ -2,4 +2,7 @@ from django import forms
 from .models import Cliente
 
 class ClienteForm(forms.Form):
-    cliente = forms.ModelChoiceField(queryset=Cliente.objects.all().order_by('nombre_completo'))
+    cliente = forms.ModelChoiceField(
+        queryset=Cliente.objects.all().order_by('nombre_completo'),
+        widget=forms.Select(attrs={'class': 'form-control w-50'})
+    )
